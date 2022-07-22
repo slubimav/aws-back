bbb537f60e08866edd27c6397d8aa347b10ecd2b
Sergey Lubimov <siarhei_liubimau@epam.com>
2022-07-23 02:26:26 +0300

    feat: get product by id from db

--- product-service/lambdas/getProductsById.js
+++ product-service/lambdas/getProductsById.js
@@ -1,21 +1,65 @@
-import products from '../productList.json' assert {type: 'json'};
-
+import { Client } from 'pg';
 
 const handleResponse = (products = {}, status = 200) => ({
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: status,
-    body: JSON.stringify(products),
-  });
-  
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Origin': '*',
+  },
+  statusCode: status,
+  body: JSON.stringify(products),
+});
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const credentials = {
+  user: PG_USERNAME,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
+
+let data_export = {}, error_code = 200;
 
 export const handler = async event => {
-        const { productId } = event.pathParameters || {};
-        const product = await products.find(({ id }) => id === productId);
-        if (!product)   { return handleResponse({ message: 'Error: Product not found!' }, 404);
-          }
-    return handleResponse({ ...product }, 200);
-};
+  
+  console.log(event);
+
+  const { productId } = event.pathParameters || {};
+
+  const client = new Client(credentials);
+
+  client.on('error', err => {
+                            data_export = 'DB Client Error 500:' + err.stack;
+                            error_code = 500;
+                            console.error('DB Client Error 500:', err.stack);
+                            })
+
+  await client
+          .connect()
+          .then(() => console.log('Client connected'))
+          .catch(err => {data_export = 'DB connection error:' + err.stack; error_code = 500})
+
+
+ await client
+          .query(`SELECT products.*, stocks.count \
+                        FROM products LEFT JOIN stocks \
+                        ON products.id = stocks.product_id\
+                        WHERE products.id='${productId}'`)
+
+          .then(res => { data_export = res.rows })
+          .catch(err => { data_export = 'DB query error 500:' + err.stack; error_code = 500})   
+  
+  await client
+          .end()
+          .then(() => console.log('Client disconnected'))
+          .catch(err => {data_export = 'DB disconnection error 500:' + err.stack; error_code = 500})
+
+  if (JSON.stringify(data_export) == JSON.stringify([])) { data_export = 'Product not found 500: Wrong id'; error_code = 400 }
+
+  return await handleResponse(data_export, error_code);
