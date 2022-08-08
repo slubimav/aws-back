8731ee36161164ebe8b1993e9ca008fd84d2ea91
Sergey Lubimov <siarhei_liubimau@epam.com>
2022-08-08 11:29:28 +0300

    feat: add catalogBatchProcess

---
+++ product-service/lambdas/catalogBatchProcess.js
@@ -0,0 +1,69 @@
+import { Client } from 'pg';
+import AWS from 'aws-sdk';
+import { handleResponse } from '../../libs/handleResponse';
+import { httpStatus } from '../../libs/httpStatus';
+import {createNewProduct} from '../../models/createNewProduct';
+
+export const handler = async event => {
+  
+const { SNS_ARN, LOW_PRICE_LIMIT, HIGHT_PRICE_NOTIFICATION, LOW_PRICE } = process.env;
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+  
+await asyncForEach(event.Records, async (record) => {
+
+try {
+
+      const messages = JSON.parse(record.body);
+
+      console.log(messages);
+
+      const priceNotification = messages.price > process.env.LOW_PRICE_LIMIT ? process.env.HIGHT_PRICE_NOTIFICATION : process.env.LOW_PRICE_NOTIFICATION;
+
+      const {title, description, price, count } = messages;
+   
+      const resultCreateProduct = await createNewProduct(title, description, price, count );
+
+      console.log(resultCreateProduct);
+
+      let params = {
+              
+                Subject: `Product ${messages.title} added to db. Send ${priceNotification} notification`,
+      
+                Message: JSON.stringify(messages),
+              
+                TopicArn: process.env.SNS_ARN,
+              
+                MessageAttributes: {
+              
+                Price: {
+              
+                    DataType: 'String',
+              
+                    StringValue: priceNotification
+                    },
+                }
+              };
+
+// Create promise and SNS service object
+let publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31', region: 'eu-central-1'}).publish(params).promise();
+
+// Handle promise's fulfilled/rejected states
+publishTextPromise
+ .then( data => {
+   console.log(`Product ${messages.title} added to database. Notification ${priceNotification} sent to the topic ${params.TopicArn}`)
+   console.log("MessageID is " + data.MessageId) })
+ .catch( error => console.log(error));
+
+
+return handleResponse("done", httpStatus.OK);
+  } catch (error) {
+    return handleResponse(error, httpStatus.SERVER_ERROR);
+  }
+        });
+}; 
---
+++ product-service/libs/httpStatus.js
@@ -0,0 +1,9 @@
+const httpStatus = {
+    OK          : 200,
+    SERVER_ERROR: 500,
+    NOT_FOUND   : 404,
+    BAD_REQUEST : 400,
+    UNKNOW_ERROR: 520,
+};
+
+export {httpStatus};
